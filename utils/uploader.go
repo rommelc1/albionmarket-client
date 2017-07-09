58f3a656d5d8d4b7fb943a3849217d68ff9cd77d
tim <tim@tim.co>
2017-07-09 11:06:41 +0700

    a

--- utils/uploader.go
+++ utils/uploader.go
@@ -16,8 +16,8 @@ func SendMarketItems(marketItems []string, ingestUrl string, locationId string) 
 	client := &http.Client{}
 
 	injestRequest := InjestRequest{
-		MarketItems: marketItems,
-		LocationId: locationId,
+		marketitems: marketItems,
+		locationid: locationId,
 	}
 
 	data, err := json.Marshal(injestRequest)
